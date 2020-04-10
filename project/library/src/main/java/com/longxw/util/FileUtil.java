37b22cec09f947b89a19c9888c5899c3048d1ab7
longxw <longxw@minstone.com.cn>
2020-04-10 14:00:21 +0800

    IO 工具类

---
+++ project/library/src/main/java/com/longxw/util/FileUtil.java
@@ -0,0 +1,23 @@
+package com.longxw.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author longxw
+ * @since 2020/4/10
+ */
+public class FileUtil {
+
+    public static String readBytesAsString(InputStream is) throws IOException {
+        return readBytesAsString(is, StandardCharsets.UTF_8);
+    }
+
+    public static String readBytesAsString(InputStream is, Charset charset) throws IOException {
+        byte[] bytes = new byte[is.available()];
+        is.read(bytes);
+        return new String(bytes, charset);
+    }
+}
