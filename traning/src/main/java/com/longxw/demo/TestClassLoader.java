71027e282e1df816e18e5a7cdf33a3b5f037c531
longxw <longxw@minstone.com.cn>
2020-04-21 10:02:09 +0800

    test read resource file in jar

--- traning/src/main/java/com/longxw/demo/TestClassLoader.java
+++ traning/src/main/java/com/longxw/demo/TestClassLoader.java
@@ -3,6 +3,10 @@ package com.longxw.demo;
 import lombok.Getter;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 
+/**
+ * @author longxw
+ * @since 2020/4/21
+ */
 public class TestClassLoader implements BeanClassLoaderAware {
 
     @Getter
