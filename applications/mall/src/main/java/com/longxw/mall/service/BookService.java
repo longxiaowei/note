df31d68148bb246692c47acae0bc39334cceb865
longxw <longxw@minstone.com.cn>
2020-05-09 15:36:11 +0800

    整合 mybatis plus

--- applications/mall/src/main/java/com/longxw/mall/service/BookService.java
+++ applications/mall/src/main/java/com/longxw/mall/service/BookService.java
@@ -2,10 +2,7 @@ package com.longxw.mall.service;
 
 import com.longxw.graphql.annotation.GraphqlQuery;
 import com.longxw.graphql.provider.DataFetcherService;
-import com.longxw.mall.lock.RedisLock;
 import com.longxw.mall.model.Book;
-import com.longxw.mall.redis.RedisUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,22 +12,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookService implements DataFetcherService {
 
-    @Autowired
-    RedisLock redisLock;
-
     @GraphqlQuery
     public Book bookById(String id, String name, Integer pageCount){
-        try {
-            this.redisLock.setIfAbsent("cs", "cv");
-            Book book = new Book();
-            book.setId(id);
-            book.setName(name);
-            book.setPageCount(pageCount);
-            return book;
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }finally {
-            RedisUtil.del("cs");
-        }
+        Book book = new Book();
+        book.setId(id);
+        book.setName(name);
+        book.setPageCount(pageCount);
+        return book;
     }
 }
--- applications/mall/src/main/java/com/longxw/mall/util/SpringBeanUtil.java
+++ applications/mall/src/main/java/com/longxw/mall/util/SpringBeanUtil.java
@@ -3,6 +3,7 @@ package com.longxw.mall.util;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,7 +16,7 @@ public class SpringBeanUtil implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
         SpringBeanUtil.applicationContext = applicationContext;
     }
 
