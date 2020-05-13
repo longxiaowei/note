28dcdcf06c4ad57f305c6245ad1781f43ca560d8
longxw <longxw@minstone.com.cn>
2020-05-13 17:11:56 +0800

    redis发布订阅器

--- applications/mall/src/main/java/com/longxw/mall/MallConfiguration.java
+++ applications/mall/src/main/java/com/longxw/mall/MallConfiguration.java
@@ -34,31 +34,34 @@ public class MallConfiguration {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory){
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate();
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
 
-        Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
+        final Jackson2JsonRedisSerializer<Object> objectJackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper om = new ObjectMapper();
         // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jacksonSeial.setObjectMapper(om);
+        objectJackson2JsonRedisSerializer.setObjectMapper(om);
 
         // 值采用json序列化
-        redisTemplate.setValueSerializer(jacksonSeial);
+        redisTemplate.setValueSerializer(objectJackson2JsonRedisSerializer);
         //使用StringRedisSerializer来序列化和反序列化redis的key值
         redisTemplate.setKeySerializer(new StringRedisSerializer());
 
         // 设置hash key 和value序列化模式
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(jacksonSeial);
+        redisTemplate.setHashValueSerializer(objectJackson2JsonRedisSerializer);
         redisTemplate.afterPropertiesSet();
 
         return redisTemplate;
     }
 
 
+    /**
+     * redis 发布订阅容器
+     */
     @Bean
     public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory,
                                                                        @Autowired List<AbstractRedisListener> list){
